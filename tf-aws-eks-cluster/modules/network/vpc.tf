c4e80d82cc5e0ca47f331a63db6ac81011d8890d
Vitor Lobo <lobocode@gmail.com>
2021-09-06 07:51:24 -0300

    add cluster roles

---
+++ tf-aws-eks-cluster/modules/network/vpc.tf
@@ -0,0 +1,9 @@
+resource aws_vpc cluster_vpc {
+    cidr_block = ""
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+        Name = format("%s-vpc", var.cluster_name)
+    }
+}
