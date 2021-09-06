3e035796b1502e38b079eddc9941bde43da85892
Vitor Lobo <lobocode@gmail.com>
2021-09-06 07:59:47 -0300

    add cluster vpc and subnets in modules

--- tf-aws-eks-cluster/modules.tf
+++ tf-aws-eks-cluster/modules.tf
@@ -2,4 +2,14 @@ module cluster {
     source = "./modules/cluster"
     cluster_name = var.cluster_name 
     aws_region = var.aws_region
+
+    cluster_vpc   = module.network.cluster_vpc
+    private_subnet_1a   = module.network.private_subnet_1a
+    private_subnet_1c   = module.network.private_subnet_1c
+}
+
+module network {
+    source = "./modules/network"
+    cluster_name = var.cluster_name
+    aws_region = var.aws_region
 }
