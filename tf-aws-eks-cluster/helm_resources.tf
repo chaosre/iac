e4c7ace6f89e7cfa5a1c9418e25ab8b6308f301e
Vitor Lobo <lobocode@gmail.com>
2021-09-08 07:59:55 -0300

    add thanos resource

--- tf-aws-eks-cluster/helm_resources.tf
+++ tf-aws-eks-cluster/helm_resources.tf
@@ -2,14 +2,30 @@
 resource "helm_release" "grafana" {
     name = "grafana"
     chart = "grafana"
-    namespace = ""
+    namespace = "monitoring"
 
     values = [
-        "${file("../helm/grafana/values.yaml")}"
+        "${file("./helm/grafana/values.yaml")}"
     ]
 
     set {
         name  = "Name"
         value = "Grafana"
+    }
+}
+
+// Thanos
+resource "helm_release" "thanos" {
+    name = "thanos"
+    chart = "thanos"
+    namespace = "metrics"
+
+    values = [
+        "${file("./helm/thanos/values.yaml")}"
+    ]
+
+    set {
+        name  = "Name"
+        value = "Thanos"
     }
 }
