dc8d36f2b1a19c357c80e9c412488b6cadc56737
Vitor Lobo <lobocode@gmail.com>
2021-09-09 07:40:27 -0300

    add LDAPFORM and make response

--- web-ldap-query/server.py
+++ web-ldap-query/server.py
@@ -5,9 +5,15 @@ import sys
 from urllib.parse import urlparse
 
 # Flask
-from flask import Flask, request, redirect, url_for, render_template_string
+from flask import (
+    Flask, request, redirect, url_for, render_template_string,
+    make_response
+)
 # https://flask-login.readthedocs.io/en/latest/
 from flask_login import LoginManager, current_user
+
+# https://flask-ldap3-login.readthedocs.io/en/latest/quick_start.html
+from flask_ldap3_login.forms import LDAPLoginForm
 
 
 app = Flask(__name__)
@@ -23,12 +29,32 @@ def home():
     # Redirecting users who are not logged in
     if not current_user or current_user.is_anonymous:
         return redirect(url_for('login'))
+    
+    # if logged
+    template = """
+    <h1>Welcome: {{ current_user.username }}</h1>
+    <h2>{{ current_user.dn }}</h2>
+    """
+    return render_template_string(template)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     target_url = url_path(request.args.get('url', '/'))
     if current_user.is_authenticated:
         return redirect(target_url)
+    form = LDAPLoginForm()
+    if form.validate_on_submit():
+        user = form.user
+        # flask_login stores user in session
+        login_user(user)
+        if user.groups:
+            identify = {'uisername': user.username, 'groups': user.groups}
+        else:
+            identify = user.username
+
+        response = make_response(redirect(target_url))
+        return response
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
