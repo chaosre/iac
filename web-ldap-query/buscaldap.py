ff9c4270fb3ec519562e3224f91b97b9182b68f6
Vitor Lobo <lobocode@gmail.com>
2021-09-10 22:26:43 -0300

    wtf

--- web-ldap-query/buscaldap.py
+++ web-ldap-query/buscaldap.py
@@ -2,29 +2,166 @@
 
 import flask
 from flask import Flask, request, flash
-from shelljob import proc
+#from shelljob import proc
+import os
 
 app = Flask(__name__)
 
 PAGE = """
-    <html>
-        <head>
-            <title>Buscaldap</title>
-        </head>
-        <body>
-            <form action="/process" method="POST">
-                <label>Racf</label>
-                <input name="racf" />
-                <br/><br/>
-                <label>Senha</label>
-                <input name="senha" />
-                <br/><br/>
-                <label>GrupoAD</label>
-                <input name="grupoad" />
-                <br/><br/>
-                <input type="submit" />                
-            </form>
-    </html>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
+
+* {
+    margin: 0;
+    border: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+}
+
+body {
+    background: linear-gradient(45deg, #e2a32d, #e06c00);
+    background-repeat: no-repeat;
+    min-height: 100vh;
+    min-width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+main.container {
+    background: white;
+    min-width: 320px;
+    min-height: 40vh;
+    padding: 2rem;
+    box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+}
+
+main h2 {
+    font-weight: 600;
+    margin-bottom: 2rem;
+    position: relative;
+}
+
+main h2::before {
+    content: '';
+    position: absolute;
+    height: 4px;
+    width: 25px;
+    bottom: 3px;
+    left: 0;
+    border-radius: 8px;
+    background: linear-gradient(45deg, #e2a32d, #e06c00);
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.input-field {
+    position: relative;
+}
+
+form .input-field {
+    margin-bottom: 1.5rem;
+}
+
+.input-field .underline::before {
+    content: '';
+    position: absolute;
+    height: 1px;
+    width: 100%;
+    bottom: -5px;
+    left: 0;
+    background: rgba(0, 0, 0, 0.2);
+}
+
+.input-field .underline::after {
+    content: '';
+    position: absolute;
+    height: 1px;
+    width: 100%;
+    bottom: -5px;
+    left: 0;
+    background: linear-gradient(45deg, #e2a32d, #e06c00);
+    transform: scaleX(0);
+    transition: all .3s ease-in-out;
+    transform-origin: left;
+}
+
+.input-field input:focus ~ .underline::after {
+    transform: scaleX(1);
+}
+
+.input-field input {
+    outline: none;
+    font-size: 0.9rem;
+    color: rgba(0, 0, 0, 0.7);
+    width: 100%;
+}
+
+.input-field input::placeholder {
+    color: rgba(0, 0, 0, 0.5);
+}
+
+form input[type="submit"] {
+    margin-top: 2rem;
+    padding: 0.4rem;
+    width: 100%;
+    background: linear-gradient(to left, #e2a32d, #e06c00);
+    cursor: pointer;
+    color: white;
+    font-size: 0.9rem;
+    font-weight: 300;
+    border-radius: 4px;
+    transition: all 0.3s ease;
+}
+
+form input[type="submit"]:hover {
+    letter-spacing: 0.5px;
+    background: linear-gradient(to right, #e2a32d, #e06c00);
+}
+
+.footer {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+}
+
+</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/1ab94d0eba.js" crossorigin="anonymous"></script>
+    <title>BuscaLDAP</title>
+</head>
+<body>
+    <main class="container">
+        <h2>BuscaLDAP</h2>
+        <form action="process" method="POST">
+            <div class="input-field">
+                <input type="text" name="racf" id="racf" placeholder="Digite seu RACF">
+                <div class="underline"></div>
+            </div>
+            <div class="input-field">
+                <input type="password" name="senha" id="senha" placeholder="Digite sua senha">
+                <div class="underline"></div>
+            </div>
+            <div class="input-field">
+                <input type="text" name="grupoad" id="grupoad" placeholder="Digite seu GrupoAD">
+                <div class="underline"></div>
+            </div>
+            <input type="submit" value="Continue">
+        </form>
+        <div class="footer">
+        </div>
+    </main>
+</body>
+</html>
 """
 
 @app.route( '/' )
@@ -37,16 +174,13 @@ def process_form():
     senha = request.form['senha']
     grupoad = request.form['grupoad']
 
-    proc_group = proc.Group()
-    run_shell = proc_group.run( [ "bash", "-c", "uname %s %s %s" % (racf,senha,grupoad) ] )
+    stream = os.popen('uname %s %s %s' % (racf, senha, grupoad))
+    output = stream.read()
 
-    def read_process():
-        while proc_group.is_pending():
-            lines = proc_group.readlines()
-            for proc, line in lines:
-                yield line
+    data_result = "<body style='background: linear-gradient(45deg, #e2a32d, #e06c00); min-height: 100vh; min-width: 100vw; display: flex; align-items: center; justify-content: center;'><main class='container', style='background: white; min-width: 1em; padding: 2rem; padding: 2rem; box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2); border-radius: 4px;'>{}</main></body>"
 
-    return flask.Response( read_process(), mimetype= 'text/plain' )
+    return data_result.format(output)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
